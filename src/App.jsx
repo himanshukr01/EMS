73c03b1649011c1a26dac89fc91636e2a863eced
Himanshu kumar <himanshukr1505892@gmail.com>
2024-12-29 11:45:28 +0530

    random commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,32 @@ import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
 import { getLocalStorage, setLocalStorage } from './utils/localStorage'
-import { useEffect } from'react'
+import { useEffect, useState } from'react'
+import AuthContext from './context/AuthContext'
+import { useContext } from'react'
 
 const App = () => {
 
-  useEffect(() => {
-    // setLocalStorage()
-    getLocalStorage()
-  },)
-  
+    const [user, setUser] = useState(null)
+
+    const handleLogin = (email, password)=> {
+      if (email=='admin@me.com' && password=='123'){
+        setUser('admin')
+      } else if(email=='user@me.com' && password=='123'){
+        setUser('Employee')
+        }
+      else{
+        alert("Invalid credentials")
+      }
+    }
+
+    const data = useContext(AuthContext)
+    console.log(data)
 
   return (
     <>
-    <Login />
-    {/* <EmployeeDashboard /> */}
-    {/* <AdminDashboard /> */}
+    {!user ? <Login handleLogin={handleLogin}/>: ''}
+    {user== 'admin' ? <AdminDashboard /> : <EmployeeDashboard />}
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import AuthContext from './context/AuthContext'
-import TaskContext from './context/TaskContext'
+import AuthProvider from './context/AuthProvider.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthContext>
-      <TaskContext>
+    
+        <AuthProvider>
         <App />
-      </TaskContext>
-    </AuthContext>
+        </AuthProvider>
+      
   </StrictMode>,
 )
