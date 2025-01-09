05e0f3c5f83e7ac8cd00721b4590ff43447170e1
Himanshu kumar <himanshukr1505892@gmail.com>
2025-01-09 12:02:14 +0530

    random commit

--- src/components/other/AllTask.jsx
+++ src/components/other/AllTask.jsx
@@ -4,17 +4,33 @@ import { AuthContext } from '../../context/AuthProvider'
 const AllTask = () => {
 
     const authData = useContext(AuthContext)
-    console.log(authData)
 
-  return (
-    <div className='bg-[#1c1c1c] p-5 rouded mt-5 h-40 overflow-auto'>
-        <div className='bg-red-400 py-2 mb-2 px-4 flex justify-between rounded'>
-            <h2>Himanshu</h2>
-            <h3>Make a UI Design</h3>
-            <h5>Status</h5>
-        </div>
+    return (
+        <div className='bg-[#1c1c1c] p-5 rouded mt-5  '>
 
-        <div className='bg-green-400 py-2 mb-2 px-4 flex justify-between rounded'>
+            <div className='bg-red-400 py-2 mb-2 px-4 flex justify-between rounded'>
+                <h2 className='text-lg font-medium w-1/5 '>Employee Name</h2>
+                <h3 className='text-lg font-medium w-1/5 '>New Task</h3>
+                <h5 className='text-lg font-medium w-1/5 '>Active Task</h5>
+                <h5 className='text-lg font-medium w-1/5 '>Completed</h5>
+                <h5 className='text-lg font-medium w-1/5 '>Failed</h5>
+
+            </div>
+
+            <div className='overflow-auto'>
+            {authData.employees.map(function (elem) {
+                return <div className='border-2 border-emerald-400 py-2 mb-2 px-4 flex justify-between rounded '>
+                    <h2 className='text-lg font-medium w-1/5 text-white-600'>{elem.firstName}</h2>
+                    <h3 className='text-lg font-medium w-1/5 text-red-800'>{elem.taskCounts.active}</h3>
+                    <h5 className='text-lg font-medium w-1/5 text-green-800'>{elem.taskCounts.active}</h5>
+                    <h5 className='text-lg font-medium w-1/5 text-blue-800'>{elem.taskCounts.completed}</h5>
+                    <h5 className='text-lg font-medium w-1/5 text-yellow-400'>{elem.taskCounts.failed}</h5>
+                </div>
+            })}
+            </div>
+
+
+            {/* <div className='bg-green-400 py-2 mb-2 px-4 flex justify-between rounded'>
             <h2>Himanshu</h2>
             <h3>Make a UI Design</h3>
             <h5>Status</h5>
@@ -36,9 +52,9 @@ const AllTask = () => {
             <h2>Himanshu</h2>
             <h3>Make a UI Design</h3>
             <h5>Status</h5>
+        </div> */}
         </div>
-    </div>
-  )
+    )
 }
 
 export default AllTask
--- src/components/other/CreateTask.jsx
+++ src/components/other/CreateTask.jsx
@@ -3,40 +3,40 @@ import React from 'react'
 const CreateTask = () => {
   return (
     <div>
-       <form className='flex items-center justify-between w-full bg-[#1c1c1c] rounded-xl mt-9'>
+       <form className='flex items-center justify-between w-full bg-[#1c1c1c] rounded-xl mt-9  '>
         <div className='p-5 '>
           
           <div className='p-2'>
           <h3>Task Title</h3>
-          <input type="text" placeholder='Make a UI Design' className='rounded-md w-[575px] bg-black' />
+          <input type="text" placeholder='Make a UI Design' className='rounded-md w-[575px] bg-black border-2 w-[45vw]' />
           </div>
 
           <div className='p-2'>
             <h3>Date</h3>
-            <input type="date" name="" id="" className='rounded-md w-[575px] bg-black' />
+            <input type="date" name="" id="" className='rounded-md w-[575px] bg-black border-2 w-[45vw]' />
           </div>
 
           <div className='p-2'>
             <h3>Assign To</h3>
-            <input type="text" placeholder='Eployee Name' className='rounded-md w-[575px] bg-black' />
+            <input type="text" placeholder='Eployee Name' className='rounded-md w-[575px] bg-black border-2 w-[45vw]' />
           </div>
 
           <div className='p-2'>
             <h3>Category</h3>
-            <input type="text" placeholder='design, dev, etc' className='rounded-md w-[575px] bg-black'/>
+            <input type="text" placeholder='design, dev, etc' className='rounded-md w-[575px] bg-black border-2 w-[45vw]'/>
           </div>
 
         </div>
 
-        <div className='p-5'>
+        <div className='p-5 '>
 
           <div>
             <h3>Description</h3>
-            <textarea name="" id="" cols='80' rows='10' className='rounded-md bg-black border-2' ></textarea>
+            <textarea name="" id="" cols='80' rows='10' className='rounded-md bg-black border-2 w-[45vw] ' ></textarea>
           </div>
 
           <div>
-            <button className='bg-emerald-600 w-full h-[40px] rounded-md mt-3'>Create Task</button>
+            <button className='bg-emerald-600 w-full h-[40px] rounded-md mt-3 border-2 w-[45vw]'>Create Task</button>
           </div>
 
         </div>
